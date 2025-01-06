1b2186d022c846de3432d2384455f08f746d963c
Jack Kilpatrick <2549732K@student.gla.ac.uk>
2025-01-06 23:40:27 +0900

    V2: agent/env improvements, fixes and cleaned code

--- agent.py
+++ agent.py
@@ -1,57 +1,91 @@
 import torch
 from torch import nn, optim
+import numpy as np
 import copy
-
-# Approx. replay memory size for 1mil entries
-# ((84*84*4*2*1000000)*4) / (1e+6) = 225,792.0 Mb or 225 Gb
+import os
 
 class Agent(nn.Module):
 
-    def __init__(self, num_actions, device) -> None:
+    def __init__(self, num_actions: int, device: torch.device) -> None:
         super().__init__()
         self.device = device
         self.to(self.device)
 
         self._iter = 0
+        self._episode_num = 1
         self._prev_state = None
 
         self.num_actions = num_actions
         self._init_network(self.num_actions)
         self.target_network_update_frequency = 10000
-        self._target_network_update_counter = self.target_network_update_frequency
+        self._target_network_update_counter = 0
 
-        self.replay_buffer_size = 10000
+        self.replay_buffer_size = 1000000
+        self.save_replay_buffer = True
         self._init_replay_buffer(self.replay_buffer_size)
-        self.train_start_time = self.replay_buffer_size // 20
-        self._train_start_counter = self.train_start_time
+        self.train_start_time = 12500
 
         self.batch_size = 32
         self.lr = 0.00025
         self.momentum = 0.95
         self.min_squared_gradient = 0.01
         self.optim = optim.RMSprop(self.network.parameters(), lr=self.lr, momentum=self.momentum, eps=self.min_squared_gradient)
+        self.loss_fn = torch.nn.SmoothL1Loss()
 
         self.start_eps = 1
         self.end_eps = 0.1
-        self.eps_schedule_steps = 10000
+        self.eps_schedule_steps = 250000
         self._init_eps_schedule(self.start_eps, self.end_eps, self.eps_schedule_steps)
+        self.eval_eps = 0.05
 
         self.gamma = 0.99
 
-    def reset(self, action, reward):
-        if self._prev_state is not None:
-            self._add_to_replay_buffer(self._prev_state, action, reward, None)
-            self._prev_state = None
+    def reset(self, action: int, reward: float) -> None:
+        if self.training:
+            if self._prev_state is not None:
+                self._add_to_replay_buffer(self._prev_state, action, reward, None)
+                self._prev_state = None
+            self._episode_num += 1
 
-    def save(self, iter, mean_score):
+    def has_checkpoint(self) -> None:
+        return os.path.exists("agent.pt")
+
+    def save_checkpoint(self) -> None:
         torch.save({
-            "iter": iter,
+            "iter": self._iter,
+            "episode_num": self._episode_num,
+            "target_network_update_counter": self._target_network_update_counter,
             "model_state_dict": self.state_dict(),
             "optimizer_state_dict": self.optim.state_dict(),
-            "mean_score": mean_score,
         }, "agent.pt")
+        if self.save_replay_buffer:
+            torch.save({
+                "curr_replay_buffer_idx": self._curr_replay_buffer_idx,
+                "replay_buffer": self.replay_buffer,
+            }, "replay_buffer.pt")
 
-    def _init_network(self, num_actions):
+    def load_checkpoint(self) -> tuple[int, int]:
+        checkpoint = torch.load(
+            "agent.pt",
+            weights_only=True,
+        )
+        if self.training and os.path.exists("replay_buffer.pt"):
+            replay_buffer_checkpoint = torch.load(
+                "replay_buffer.pt",
+                weights_only=True,
+            )
+            self.replay_buffer = replay_buffer_checkpoint["replay_buffer"]
+            self._curr_replay_buffer_idx = replay_buffer_checkpoint["curr_replay_buffer_idx"]
+        self.load_state_dict(checkpoint["model_state_dict"])
+        self.optim.load_state_dict(checkpoint["optimizer_state_dict"])
+        self._iter = checkpoint["iter"]
+        self._episode_num = checkpoint["episode_num"]
+        self._target_network_update_counter = checkpoint["target_network_update_counter"]
+        self._update_current_eps()
+        return self._iter, self._episode_num
+
+
+    def _init_network(self, num_actions: int) -> None:
         self.network = nn.Sequential(
             nn.Conv2d(in_channels=4, out_channels=32, kernel_size=8, stride=4),
             nn.ReLU(),
@@ -66,24 +100,27 @@ class Agent(nn.Module):
         ).to(self.device)
         self.target_network = copy.deepcopy(self.network)
 
-    def _init_replay_buffer(self, replay_buffer_size):
-        self.replay_buffer = [None for i in range(replay_buffer_size)]
+    def _init_replay_buffer(self, replay_buffer_size: int) -> None:
+        self.replay_buffer = [None for _ in range(replay_buffer_size)]
         self._curr_replay_buffer_idx = 0
     
-    def _add_to_replay_buffer(self, prev_state, action, reward, state):
-        self.replay_buffer[self._curr_replay_buffer_idx] = (prev_state, action, reward, state)
-        self._curr_replay_buffer_idx = (self._curr_replay_buffer_idx + 1) % len(self.replay_buffer)
+    def _add_to_replay_buffer(self, prev_state: torch.Tensor, action: int, reward: float, state: torch.Tensor) -> None:
+        prev_state = (prev_state * 255).to(device=torch.device("cpu"), dtype=torch.uint8)
+        action = action
+        reward = reward
+        state = None if state is None else (state * 255).to(device=torch.device("cpu"), dtype=torch.uint8)
 
-    def _sample_from_replay_buffer(self, batch_size):
+        self.replay_buffer[self._curr_replay_buffer_idx] = (prev_state, action, reward, state)
+        self._curr_replay_buffer_idx = (self._curr_replay_buffer_idx + 1) % self.replay_buffer_size
+
+    def _sample_from_replay_buffer(self, batch_size: int) -> None:
         prev_states = []
         actions = []
         rewards = []
         nonterminal_states = []
         nonterminal_states_mask = []
 
-        nonempty_buffer_idxs = [idx for idx in range(len(self.replay_buffer)) if self.replay_buffer[idx] is not None]
-        for batch_idx in range(batch_size):
-            sample_idx = nonempty_buffer_idxs[torch.randint(high=len(nonempty_buffer_idxs), size=(1, )).item()]
+        for batch_idx, sample_idx in enumerate(torch.randint(high=min(self._iter, self.replay_buffer_size), size=(batch_size, ))):
             prev_state, action, reward, state = self.replay_buffer[sample_idx]
             prev_states.append(prev_state)
             actions.append(action)
@@ -91,26 +128,29 @@ class Agent(nn.Module):
             if state is not None:
                 nonterminal_states.append(state)
                 nonterminal_states_mask.append(batch_idx)
-        prev_states = torch.stack(prev_states)
+        prev_states = (torch.stack(prev_states)/255.0).to(self.device)
         actions = torch.tensor(actions).to(self.device)
         rewards = torch.tensor(rewards).to(self.device)
-        nonterminal_states = torch.stack(nonterminal_states)
+        nonterminal_states = (torch.stack(nonterminal_states)/255.0).to(self.device)
         nonterminal_states_mask = torch.tensor(nonterminal_states_mask).to(self.device)
         batch = (prev_states, actions, rewards, nonterminal_states, nonterminal_states_mask)
         return batch
+    
+    def get_curr_eps(self) -> torch.Tensor:
+        return self._curr_eps if self.training else torch.tensor(self.eval_eps)
 
-    def _init_eps_schedule(self, start_eps, end_eps, eps_schedule_steps):
+    def _init_eps_schedule(self, start_eps: float, end_eps: float, eps_schedule_steps: int) -> None:
         self._eps_schedule = torch.linspace(start=start_eps, end=end_eps, steps=eps_schedule_steps)
-        self._curr_eps_idx = 0
         self._update_current_eps()
 
-    def _update_current_eps(self):
-        self.curr_eps = self._eps_schedule[self._curr_eps_idx]
-        self._curr_eps_idx += 1
+    def _update_current_eps(self) -> None:
+        if self._iter < self.eps_schedule_steps:
+            self._curr_eps = self._eps_schedule[self._iter]
+        else:
+            self._curr_eps = self._eps_schedule[-1]
 
-    def train(self):
-
-        if self._train_start_counter > 0:
+    def train_step(self) -> None:
+        if self._iter < self.train_start_time:
             return
 
         batch = self._sample_from_replay_buffer(self.batch_size)
@@ -123,36 +163,31 @@ class Agent(nn.Module):
         prev_qs = self.network(prev_states)[torch.arange(self.batch_size), actions]
 
         self.optim.zero_grad()
-        loss = torch.mean((targets-prev_qs)**2)
+        loss = self.loss_fn(prev_qs, targets)
         loss.backward()
         self.optim.step()
 
-        self._target_network_update_counter -= 1
-        if self._target_network_update_counter == 0:
+        if self._target_network_update_counter < self.target_network_update_frequency:
+            self._target_network_update_counter += 1
+        else:
             self.target_network = copy.deepcopy(self.network)
-            self._target_network_update_counter = self.target_network_update_frequency
+            self._target_network_update_counter = 0
 
-    def forward(self, state, action, reward):
+    def forward(self, state: torch.Tensor, action: int, reward: float) -> int | tuple[int, torch.Tensor]:
 
-        self._iter += 1
+        if self.training:
+            if self._prev_state is not None:
+                self._add_to_replay_buffer(self._prev_state, action, reward, state)
+            self._prev_state = state
         
-        if self._prev_state is not None:
-            self._add_to_replay_buffer(self._prev_state, action, reward, state)
-        self._prev_state = state
-
-        if self._train_start_counter > 0:
-            self._train_start_counter -= 1
-            return torch.randint(high=self.num_actions, size=(1, )).item()
-        
-        if torch.bernoulli(self.curr_eps) == 1:
-            return torch.randint(high=self.num_actions, size=(1, )).item()
+        if self._iter < self.train_start_time or torch.bernoulli(self.get_curr_eps()) == 1:
+            action = torch.randint(high=self.num_actions, size=(1, )).item()
         else:
             qs = self.network(state.unsqueeze(0))
             action = torch.argmax(qs, -1).squeeze(0).item()
-        self._update_current_eps()
-        return action
 
-if __name__ == "__main__":
-    replay_size = 10000
-    replay_mem_size = ((84*84*4*2*replay_size)*4) / (1e+6)
-    print(replay_mem_size)
+        if self.training:
+            self._iter += 1
+            self._update_current_eps()
+            
+        return action
--- main.py
+++ main.py
@@ -2,50 +2,150 @@ import gymnasium as gym
 import ale_py
 from agent import Agent
 import torch
+import time
 import datetime
-gym.register_envs(ale_py)
+import numpy as np
+import os
+import argparse
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def print_and_log(string: str, log_file_path: str | os.PathLike | None) -> None:
+    log_string = f"{datetime.datetime.now()}: {string}"
+    print(log_string)
+    if log_file_path:
+        with open(log_file_path, "a") as f:
+            f.write(log_string + "\n")
 
-# Default v4 env doesn't handle frame flickering, so use gym wrappers to fix
-env = gym.make("BreakoutNoFrameskip-v4", render_mode="human")
-env = gym.wrappers.AtariPreprocessing(env, scale_obs=True)
-env = gym.wrappers.FrameStackObservation(env, stack_size=4)
-env = gym.wrappers.NumpyToTorch(env, device=device)
-agent = Agent(num_actions=env.action_space.n.item(), device=device)
+def train(agent: Agent, env: gym.Env, num_train_iters:int = 12500000, num_eval_iters:int = 25000, train_freq:int = 4, eval_freq:int = 2500, use_checkpoint:bool = True, checkpoint_freq: int = 12500, log_file_path: str | os.PathLike = None, rewards_file_path: str | os.PathLike = "eval_mean_rewards_per_episode.npy") -> None:
 
-num_iters = 10000000
-agent_train_freq = 4
-observation, info = env.reset()
-action, reward = (0, 0)
-episode_num = 1
-total_score = 0
-score = 0
-for iter in range(num_iters):
+    agent.train()
+    start_iter = 0
+    episode_num = 1
+    eval_mean_rewards_per_episode = []
+    if use_checkpoint:
+        if agent.has_checkpoint():
+            print_and_log(f": Found checkpoint. Loading...", log_file_path=log_file_path)
+            start_iter, episode_num = agent.load_checkpoint()
+            print_and_log(f"Continuing from iteration {start_iter}", log_file_path=log_file_path)
+        if os.path.exists(rewards_file_path):
+            with open(rewards_file_path, "rb") as f:
+                eval_mean_rewards_per_episode = np.load(f).tolist()
 
-    action = agent(observation, action, reward)
-    if (iter % agent_train_freq) == 0:
-        agent.train()
+    observation, info = env.reset()
+    action, reward = (0, 0)
+    episode_reward = 0
+    episode_start_time = time.time()
+    for iter in range(start_iter, num_train_iters):
 
-    observation, reward, terminated, truncated, info = env.step(action)
-    score += reward
+        action = agent(observation, action, reward)
 
-    if terminated or truncated:
+        if (iter % train_freq) == 0:
+            agent.train_step()
 
-        total_score += score
-        mean_score = total_score / episode_num
-        log_str = f"{datetime.datetime.now()}, episode_num: {episode_num}, episode score: {score}, iter: {iter}, mean_score: {mean_score}"
-        print(log_str)
-        with open("log.txt", "a") as f:
-            f.write(log_str + "\n")
-        if (iter % 10000) == 0:
-            agent.save(iter, mean_score)
+        observation, reward, terminated, truncated, info = env.step(action)
+        episode_reward += reward
 
-        score = 0
-        episode_num += 1
+        if use_checkpoint and ((iter % checkpoint_freq) == 0):
+            print_and_log(f"Saving checkpoint at iteration {iter}...", log_file_path=log_file_path)
+            agent.save_checkpoint()
 
-        agent.reset(action, reward)
+        if terminated or truncated:
+            episode_end_time = time.time()
+            episode_time = episode_end_time - episode_start_time
+            episode_log_string = f"episode_num: {episode_num}, episode_time: {episode_time:.2f}s, episode reward: {episode_reward:.2f}, iter: {iter}"
+            print_and_log(episode_log_string, log_file_path=log_file_path)
+
+            episode_num += 1
+            episode_reward = 0
+
+        if (iter % eval_freq) == 0:
+            print_and_log(f"Iteration {iter} reached, evaluating...", log_file_path=log_file_path)
+            mean_reward_per_episode = eval(agent, env, num_iters=num_eval_iters, log_file_path=log_file_path, summarise_episodes=False)
+            
+            eval_mean_rewards_per_episode.append(mean_reward_per_episode)
+            
+            with open(rewards_file_path, "wb") as f:
+                np.save(f, np.array(eval_mean_rewards_per_episode))
+
+            print_and_log("Saving evaluation results...", log_file_path=log_file_path)
+
+            agent.train()
+
+        if terminated or truncated or ((iter % eval_freq) == 0):
+            agent.reset(action, reward)
+            observation, info = env.reset()
+            action, reward = (0, 0)
+            episode_start_time = time.time()
+
+def eval(agent: Agent, env: gym.Env, num_iters: int = 100000, use_checkpoint: bool = True, log_file_path: str | os.PathLike = None, summarise_episodes: bool = True) -> np.ndarray:
+
+    agent.eval()
+    if use_checkpoint and agent.has_checkpoint():
+        agent.load_checkpoint()
+
+    total_reward = 0
+
+    with torch.no_grad():
+
         observation, info = env.reset()
         action, reward = (0, 0)
+        episode_num = 1
+        episode_reward = 0
+        episode_start_time = time.time()
+        for iter in range(num_iters):
 
-env.close()
+            action = agent(observation, action, reward)
+
+            observation, reward, terminated, truncated, info = env.step(action)
+            episode_reward += reward
+            total_reward += reward
+
+            if terminated or truncated:
+                episode_end_time = time.time()
+                episode_time = episode_end_time - episode_start_time
+                if summarise_episodes:
+                    episode_log_string = f"episode_num: {episode_num}, episode_time: {episode_time:.2f}s, episode reward: {episode_reward:.2f}, iter: {iter}"
+                    print_and_log(episode_log_string, log_file_path=log_file_path)
+
+                episode_num += 1
+                episode_reward = 0
+                agent.reset(action, reward)
+                observation, info = env.reset()
+                action, reward = (0, 0)
+                episode_start_time = time.time()
+
+        mean_reward_per_episode = total_reward / episode_num
+        eval_string = f"Evaluation complete. Obtained mean reward of {mean_reward_per_episode:.2f} across {episode_num} episodes"
+        print_and_log(eval_string, log_file_path=log_file_path)
+        return mean_reward_per_episode
+    
+def main() -> None:
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--eval", action="store_true", help="evaluate pre-trained agent")
+    parser.add_argument("--render", action="store_true", help="render environment")
+    args = parser.parse_args()
+    train_mode = not args.eval
+    should_render = args.render
+
+    torch.manual_seed(2024)
+    gym.register_envs(ale_py)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Default v4 env doesn't handle frame flickering, so use gym wrappers to fix
+    env = gym.make("BreakoutNoFrameskip-v4", render_mode="human" if (not train_mode) or should_render else None)
+    env = gym.wrappers.AtariPreprocessing(env, scale_obs=True)
+    env = gym.wrappers.FrameStackObservation(env, stack_size=4)
+    env = gym.wrappers.ClipReward(env, min_reward=-1, max_reward=1)
+    env = gym.wrappers.NumpyToTorch(env, device=device)
+
+    agent = Agent(num_actions=env.action_space.n.item(), device=device)
+
+    
+    if train_mode:
+        train(agent, env, log_file_path="train_log.txt")
+    else:
+        eval(agent, env, log_file_path="eval_log.txt")
+    env.close()
+
+if __name__ == "__main__":
+    main()
