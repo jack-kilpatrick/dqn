04dcbdfb670cd42276e4dd57a0c5c522e16bcc6b
Jack Kilpatrick <2549732K@student.gla.ac.uk>
2025-01-25 22:24:39 +0900

    V3: Video recordings, command line arguments, agent documentation and code refactoring

--- agent.py
+++ agent.py
@@ -1,22 +1,67 @@
 import torch
 from torch import nn, optim
-import numpy as np
+import gymnasium as gym
 import copy
 import os
 
 class Agent(nn.Module):
-
-    def __init__(self, num_actions: int, device: torch.device) -> None:
+    """Base agent class"""
+    
+    def __init__(self, name:str, env: gym.Env, device: torch.device) -> None:
+        "Initialise an agent, setting the name and environment name and moving the module to the specified device"
         super().__init__()
+        self.name = name
+        self.env_name = env.unwrapped.spec.id
         self.device = device
         self.to(self.device)
+        
+    def train_mode(self) -> None:
+        """Set relevant internal attributes to training mode e.g. for maintaining a replay buffer or updating network batchnorm layers"""
+        raise NotImplementedError
+
+    def eval_mode(self) -> None:
+        """Set relevant internal atttributes to training mode e.g. disabling training exploration strategy"""
+        raise NotImplementedError
+
+    def reset(self, reward: float) -> None:
+        """Perform any internal operations needed at the end of an episode"""
+        raise NotImplementedError
+    
+    def has_checkpoint(self) -> None:
+        """Return true if the necessary agent checkpoint files are present"""
+        raise NotImplementedError
+
+    def save_checkpoint(self, iter: int, episode_num: int, best_mean_reward_per_episode: float|None = None) -> None:
+        """Save necessary agent checkpoint files, mark the current iteration, episode number and best mean episodic reward for training resumption"""
+        raise NotImplementedError
+
+    def load_checkpoint(self) -> tuple[int, int, int]:
+        """Load necessary agent checkpoint files, return the iteration, episode and best mean episode reward achieved during training"""
+        raise NotImplementedError
+
+    def train_step(self) -> None:
+        """Run one step of the agents training process"""
+        raise NotImplementedError
+
+    def forward(self, state: torch.Tensor, reward: float) -> torch.Tensor:
+        """Predict an action given the current state. Also given the previous timestep reward as per typical MDP conventions"""
+        raise NotImplementedError
+
+class DQN(Agent):
+
+    def __init__(self, name: str, env: gym.Env, device: torch.device) -> None:
+        "Initialise a DQN agent with the default network and hyperparameters set according to the original paper"
+        super().__init__(name, env, device)
+
+        self._init_checkpoint_paths()
 
         self._iter = 0
         self._episode_num = 1
         self._prev_state = None
 
-        self.num_actions = num_actions
-        self._init_network(self.num_actions)
+        self.observation_space = env.observation_space
+        self.action_space = env.action_space
+        self._init_network(self.observation_space, self.action_space)
         self.target_network_update_frequency = 10000
         self._target_network_update_counter = 0
 
@@ -41,37 +86,62 @@ class Agent(nn.Module):
 
         self.gamma = 0.99
 
-    def reset(self, action: int, reward: float) -> None:
+    def train_mode(self) -> None:
+        """Set the module to training mode"""
+        if not self.training:
+            self.train()
+
+    def eval_mode(self) -> None:
+        """Set the module to evaluation mode"""
+        if self.training:
+            self.eval()
+
+    def reset(self, reward: float) -> None:
+        """Add the episode terminal transition to the replay buffer"""
         if self.training:
             if self._prev_state is not None:
-                self._add_to_replay_buffer(self._prev_state, action, reward, None)
+                self._add_to_replay_buffer(self._prev_state, self._prev_action, reward, None)
                 self._prev_state = None
             self._episode_num += 1
 
-    def has_checkpoint(self) -> None:
-        return os.path.exists("agent.pt")
+    def _init_checkpoint_paths(self) -> None:
+        """Setup checkpoint paths for both the agent and replay buffer"""
+        self.checkpoint_dir = os.path.join(os.getcwd(), "checkpoints", self.env_name, self.name)
+        if not os.path.exists(self.checkpoint_dir):
+            os.makedirs(self.checkpoint_dir)
+        self._agent_checkpoint_path = os.path.join(self.checkpoint_dir, "agent.pt")
+        self._replay_buffer_checkpoint_path = os.path.join(self.checkpoint_dir, "replay_buffer.pt")
+        self.checkpoint_paths = [self._agent_checkpoint_path, self._replay_buffer_checkpoint_path]
 
-    def save_checkpoint(self) -> None:
+    def has_checkpoint(self) -> None:
+        """Check if the agent checkpoint exists. The replay buffer is not considered here since it will be reinitialised if unavailable"""
+        return os.path.exists(self._agent_checkpoint_path)
+
+    def save_checkpoint(self, iter: int, episode_num: int, best_mean_reward_per_episode: float|None = None) -> None:
+        """Save the agent and replay buffer checkpoint"""
         torch.save({
-            "iter": self._iter,
-            "episode_num": self._episode_num,
+            "iter": iter,
+            "episode_num": episode_num,
+            "best_mean_reward_per_episode": best_mean_reward_per_episode,
             "target_network_update_counter": self._target_network_update_counter,
             "train_start_counter": self._train_start_counter,
             "model_state_dict": self.state_dict(),
             "optimizer_state_dict": self.optim.state_dict(),
-        }, "agent.pt")
+        }, self._agent_checkpoint_path)
         if self.save_replay_buffer:
             torch.save({
                 "curr_replay_buffer_idx": self._curr_replay_buffer_idx,
                 "replay_buffer_num_elts": self._replay_buffer_num_elts,
                 "replay_buffer": self.replay_buffer,
-            }, "replay_buffer.pt")
+            }, self._replay_buffer_checkpoint_path)
 
-    def load_checkpoint(self) -> tuple[int, int]:
+    def load_checkpoint(self) -> tuple[int, int, int]:
+        """Load the agent checkpoint. Attempt to load the replay buffer checkpoint if in training mode, if missing or corrupted refill before training resumption"""
         checkpoint = torch.load(
-            "agent.pt",
+            self._agent_checkpoint_path,
             weights_only=True,
         )
+        best_mean_reward_per_episode = checkpoint.get("best_mean_reward_per_episode")
         self.load_state_dict(checkpoint["model_state_dict"])
         self.optim.load_state_dict(checkpoint["optimizer_state_dict"])
         self._iter = checkpoint["iter"]
@@ -83,7 +153,7 @@ class Agent(nn.Module):
         if self.training:
             try:
                 replay_buffer_checkpoint = torch.load(
-                    "replay_buffer.pt",
+                    self._replay_buffer_checkpoint_path,
                     weights_only=True,
                 )
                 self.replay_buffer = replay_buffer_checkpoint["replay_buffer"]
@@ -93,30 +163,47 @@ class Agent(nn.Module):
                 self._iter -= self.train_start_time
                 self._train_start_counter = 0
       
-        return self._iter, self._episode_num
+        return self._iter, self._episode_num, best_mean_reward_per_episode
 
 
-    def _init_network(self, num_actions: int) -> None:
-        self.network = nn.Sequential(
-            nn.Conv2d(in_channels=4, out_channels=32, kernel_size=8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
-            nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(64*7*7, 512),
-            nn.ReLU(),
-            nn.Linear(512, num_actions),
-        ).to(self.device)
+    def _init_network(self, observation_space: gym.spaces.Box, action_space: gym.spaces.Discrete) -> None:
+        """Build the agent Q-network. Assumes that 3D observations are images (RGB or frame-stacked) and builds a CNN in this case, otherwise builds an MLP"""
+        num_actions = action_space.n
+        state_contains_images = (len(observation_space.shape) == 3)
+        if state_contains_images:
+            state_num_channels = observation_space.shape[0]
+            self.network = nn.Sequential(
+                nn.Conv2d(in_channels=state_num_channels, out_channels=32, kernel_size=8, stride=4),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
+                nn.ReLU(),
+                nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
+                nn.ReLU(),
+                nn.Flatten(),
+                nn.Linear(64*7*7, 512),
+                nn.ReLU(),
+                nn.Linear(512, num_actions),
+            ).to(self.device)
+        else:
+            state_num_elts = torch.prod(torch.tensor(observation_space.shape)).int().item()
+            self.network = nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(state_num_elts, 400),
+                nn.ReLU(),
+                nn.Linear(400, 300),
+                nn.ReLU(),
+                nn.Linear(300, num_actions)
+            )
         self.target_network = copy.deepcopy(self.network)
 
     def _init_replay_buffer(self, replay_buffer_size: int) -> None:
+        """Initialise the replay buffer, and internal state to track the position of the next element and number of contained elements"""
         self.replay_buffer = [None for _ in range(replay_buffer_size)]
         self._curr_replay_buffer_idx = 0
         self._replay_buffer_num_elts = 0
     
     def _add_to_replay_buffer(self, prev_state: torch.Tensor, action: int, reward: float, state: torch.Tensor) -> None:
+        """Add one transition (prev_state, action, reward, state) to the replay buffer. Cast and move to save memory. Updates internal buffer counters"""
         prev_state = (prev_state * 255).to(device=torch.device("cpu"), dtype=torch.uint8)
         action = action
         reward = reward
@@ -128,6 +215,7 @@ class Agent(nn.Module):
             self._replay_buffer_num_elts += 1
 
     def _sample_from_replay_buffer(self, batch_size: int) -> None:
+        """Uniformly sample a mini-batch of transitions from the replay buffer"""
         prev_states = []
         actions = []
         rewards = []
@@ -150,20 +238,24 @@ class Agent(nn.Module):
         batch = (prev_states, actions, rewards, nonterminal_states, nonterminal_states_mask)
         return batch
     
-    def get_curr_eps(self) -> torch.Tensor:
+    def _get_curr_eps(self) -> torch.Tensor:
+        """Return current epsilon value used for exploration"""
         return self._curr_eps if self.training else torch.tensor(self.eval_eps)
 
     def _init_eps_schedule(self, start_eps: float, end_eps: float, eps_schedule_steps: int) -> None:
+        """Initialise linear annealing schedule for epsilon value used for exploration"""
         self._eps_schedule = torch.linspace(start=start_eps, end=end_eps, steps=eps_schedule_steps)
         self._update_current_eps()
 
     def _update_current_eps(self) -> None:
+        """Update epsilon value used for exploration according to internal linear annealing schedule"""
         if self._iter < self.eps_schedule_steps:
             self._curr_eps = self._eps_schedule[self._iter]
         else:
             self._curr_eps = self._eps_schedule[-1]
 
     def train_step(self) -> None:
+        """Using a replay minibatch, perform one optimisation step on the Smooth L1 Loss of the predicted Q-values against the target Q-values"""
         if self._train_start_counter < self.train_start_time:
             return
 
@@ -187,23 +279,28 @@ class Agent(nn.Module):
             self.target_network = copy.deepcopy(self.network)
             self._target_network_update_counter = 0
 
-    def forward(self, state: torch.Tensor, action: int, reward: float) -> int | tuple[int, torch.Tensor]:
-
+    def forward(self, state: torch.Tensor, reward: float) -> torch.Tensor:
+        """
+        Predict the Q-values of each possible action for the current state.
+        Select the maximising action with probability 1-epsilon and a random action otherwise.
+        Also pick a random action if the replay buffer has yet to be sufficiently filled according to the associated internal counters.
+        """
         if self.training:
             if self._prev_state is not None:
-                self._add_to_replay_buffer(self._prev_state, action, reward, state)
+                self._add_to_replay_buffer(self._prev_state, self._prev_action, reward, state)
             self._prev_state = state
         
-        if (self._train_start_counter < self.train_start_time and self._iter < self.train_start_time) or torch.bernoulli(self.get_curr_eps()) == 1:
-            action = torch.randint(high=self.num_actions, size=(1, )).item()
+        if (self._train_start_counter < self.train_start_time and self._iter < self.train_start_time) or torch.bernoulli(self._get_curr_eps()) == 1:
+            action = torch.randint(high=self.action_space.n, size=(1, )).item()
         else:
             qs = self.network(state.unsqueeze(0))
-            action = torch.argmax(qs, -1).squeeze(0).item()
+            action = torch.argmax(qs, -1).squeeze(0)
             
         if self.training:
             self._iter += 1
             self._update_current_eps()
             if self._train_start_counter < self.train_start_time:
                 self._train_start_counter += 1
+            self._prev_action = action
             
         return action
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import gymnasium as gym
-import ale_py
-from agent import Agent
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+from agent import Agent, DQN
 import torch
 import time
 import datetime
@@ -8,45 +9,51 @@ import numpy as np
 import os
 import argparse
 
-def print_and_log(string: str, log_file_path: str | os.PathLike | None) -> None:
+def print_and_log(string: str, log_file_path: str | os.PathLike | None, timestamp: bool = True) -> None:
+    """Print a string and write it to the given file in append mode. Optionally timestamp it (done by default)."""
     log_string = f"{datetime.datetime.now()}: {string}"
     print(log_string)
     if log_file_path:
         with open(log_file_path, "a") as f:
             f.write(log_string + "\n")
 
-def train(agent: Agent, env: gym.Env, num_train_iters:int = 25000000, num_eval_iters:int = 25000, train_freq:int = 4, eval_freq:int = 2500, use_checkpoint:bool = True, checkpoint_freq: int = 12500, log_file_path: str | os.PathLike = None, rewards_file_path: str | os.PathLike = "eval_mean_rewards_per_episode.npy") -> None:
-
-    agent.train()
+def train(agent: Agent, env: gym.Env, num_train_iters:int = 50000000, train_freq:int = 4, num_eval_iters:int = 125000, eval_freq:int = 250000, use_checkpoints:bool = True, log_file_path: str | os.PathLike = None, eval_rewards_file_path: str | os.PathLike | None = None, eval_rewards_figure_file_path: str | os.PathLike | None = None) -> Agent:
+    """
+    Train an agent in the given environment for a specified number of iterations.
+    Evaluate throughout training in a separate loop at the specified frequency and for the specified duration.
+    Optionally checkpoint the best agent according to the evaluations (done by default). 
+    Returns the trained agent.
+    """
+    agent.train_mode()
     start_iter = 0
     episode_num = 1
     eval_mean_rewards_per_episode = []
-    if use_checkpoint:
+    best_mean_reward_per_episode = -np.inf
+    if use_checkpoints:
         if agent.has_checkpoint():
             print_and_log(f": Found checkpoint. Loading...", log_file_path=log_file_path)
-            start_iter, episode_num = agent.load_checkpoint()
+            start_iter, episode_num, best_mean_reward_per_episode = agent.load_checkpoint()
+            if best_mean_reward_per_episode is None:
+                best_mean_reward_per_episode = -np.inf
             print_and_log(f"Continuing from iteration {start_iter}", log_file_path=log_file_path)
-        if os.path.exists(rewards_file_path):
-            with open(rewards_file_path, "rb") as f:
+        if os.path.exists(eval_rewards_file_path):
+            with open(eval_rewards_file_path, "rb") as f:
                 eval_mean_rewards_per_episode = np.load(f).tolist()
 
     observation, info = env.reset()
-    action, reward = (0, 0)
+    reward = 0
     episode_reward = 0
     episode_start_time = time.time()
-    for iter in range(start_iter, num_train_iters):
+    for iter in tqdm(range(start_iter, num_train_iters), initial=0, desc="Training"):
 
-        action = agent(observation, action, reward)
+        action = agent(observation, reward)
 
-        if (iter % train_freq) == 0:
+        is_train_iter = ((iter % train_freq) == 0)
+        if is_train_iter:
             agent.train_step()
 
         observation, reward, terminated, truncated, info = env.step(action)
         episode_reward += reward
-
-        if use_checkpoint and ((iter % checkpoint_freq) == 0):
-            print_and_log(f"Saving checkpoint at iteration {iter}...", log_file_path=log_file_path)
-            agent.save_checkpoint()
 
         if terminated or truncated:
             episode_end_time = time.time()
@@ -57,29 +64,49 @@ def train(agent: Agent, env: gym.Env, num_train_iters:int = 25000000, num_eval_i
             episode_num += 1
             episode_reward = 0
 
-        if (iter % eval_freq) == 0:
+        is_eval_iter = ((iter % eval_freq) == 0)
+        if is_eval_iter:
             print_and_log(f"Iteration {iter} reached, evaluating...", log_file_path=log_file_path)
+
+            agent.eval_mode()
             mean_reward_per_episode = eval(agent, env, num_iters=num_eval_iters, log_file_path=log_file_path, summarise_episodes=False)
-            
+            agent.train_mode()
+
             eval_mean_rewards_per_episode.append(mean_reward_per_episode)
-            
-            with open(rewards_file_path, "wb") as f:
-                np.save(f, np.array(eval_mean_rewards_per_episode))
+            if eval_rewards_file_path:
+                print_and_log("Saving evaluation results...", log_file_path=log_file_path)
+                with open(eval_rewards_file_path, "wb") as f:
+                    np.save(f, np.array(eval_mean_rewards_per_episode))
+                if eval_rewards_figure_file_path:
+                    plt.plot(np.arange(len(eval_mean_rewards_per_episode)))
+                    plt.ylabel("Average score per episode")
+                    plt.xlabel(f"Training epochs")
+                    plt.savefig(eval_rewards_figure_file_path)
+                    plt.close()
 
-            print_and_log("Saving evaluation results...", log_file_path=log_file_path)
+            if mean_reward_per_episode > best_mean_reward_per_episode:
+                prev_best_mean_reward_per_episode = best_mean_reward_per_episode
+                best_mean_reward_per_episode = mean_reward_per_episode
+                print_and_log(f"New best mean reward achieved ({prev_best_mean_reward_per_episode:.2f} -> {best_mean_reward_per_episode:2f}).", log_file_path=log_file_path)
+                if use_checkpoints:
+                    print_and_log(f"Saving checkpoint...", log_file_path=log_file_path)
+                    agent.save_checkpoint(iter, episode_num, best_mean_reward_per_episode=best_mean_reward_per_episode)
 
-            agent.train()
-
-        if terminated or truncated or ((iter % eval_freq) == 0):
-            agent.reset(action, reward)
+        if terminated or truncated or is_eval_iter:
+            agent.reset(reward)
             observation, info = env.reset()
-            action, reward = (0, 0)
+            reward = 0
             episode_start_time = time.time()
 
-def eval(agent: Agent, env: gym.Env, num_iters: int = 100000, use_checkpoint: bool = True, log_file_path: str | os.PathLike = None, summarise_episodes: bool = True) -> np.ndarray:
+    return agent
 
-    agent.eval()
-    if use_checkpoint and agent.has_checkpoint():
+def eval(agent: Agent, env: gym.Env, num_iters: int = 125000, use_checkpoints: bool = True, log_file_path: str | os.PathLike = None, summarise_episodes: bool = True) -> float:
+    """
+    Evaluate an agent in the given environment for a specified number of iterations.
+    Returns the mean reward per episode across the evaluation.
+    """
+    agent.eval_mode()
+    if use_checkpoints and agent.has_checkpoint():
         agent.load_checkpoint()
 
     total_reward = 0
@@ -87,13 +114,13 @@ def eval(agent: Agent, env: gym.Env, num_iters: int = 100000, use_checkpoint: bo
     with torch.no_grad():
 
         observation, info = env.reset()
-        action, reward = (0, 0)
+        reward = 0
         episode_num = 1
         episode_reward = 0
         episode_start_time = time.time()
-        for iter in range(num_iters):
+        for iter in tqdm(range(num_iters), desc="Evaluation"):
 
-            action = agent(observation, action, reward)
+            action = agent(observation, reward)
 
             observation, reward, terminated, truncated, info = env.step(action)
             episode_reward += reward
@@ -108,9 +135,9 @@ def eval(agent: Agent, env: gym.Env, num_iters: int = 100000, use_checkpoint: bo
 
                 episode_num += 1
                 episode_reward = 0
-                agent.reset(action, reward)
+                agent.reset(reward)
                 observation, info = env.reset()
-                action, reward = (0, 0)
+                reward = 0
                 episode_start_time = time.time()
 
         mean_reward_per_episode = total_reward / episode_num
@@ -121,30 +148,70 @@ def eval(agent: Agent, env: gym.Env, num_iters: int = 100000, use_checkpoint: bo
 def main() -> None:
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--eval", action="store_true", help="evaluate pre-trained agent")
-    parser.add_argument("--render", action="store_true", help="render environment")
+    parser.add_argument("--agent", type=str, default="dqn", help="Name of the agent")
+    parser.add_argument("--env", type=str, default="BreakoutNoFrameskip-v4", help="Name of gymnasium environment to use")
+    parser.add_argument("--num_train_iters", type=int, default=50000000, help="Number of training iterations/environment steps")
+    parser.add_argument("--train_freq", type=int, default=4, help="Number of iterations/environment steps per training step")
+    parser.add_argument("--num_eval_iters", type=int, default=125000, help="Number of evaluation iterations/environment steps (Note: for an isolated evaluation if --eval is also passed, otherwise for intermediate evaluations in training)")
+    parser.add_argument("--eval_freq",type=int, default=250000, help="Number of iterations/environment steps per intermediate evaluation in training")
+    parser.add_argument("--use_checkpoints", action="store_true", help="Save checkpoints during training, after every intermediate evaluation")
+    parser.add_argument("--eval", action="store_true", help="Evaluate a pre-trained agent for a given number of iterations/environment steps")
+    parser.add_argument("--render", action="store_true", help="Render the environment")
     args = parser.parse_args()
-    train_mode = not args.eval
-    should_render = args.render
+    
+    agent_name:str = args.agent
+    env_name:str = args.env
+    train_mode:int = not args.eval
+    num_train_iters:int = args.num_train_iters
+    train_freq:int = args.train_freq
+    num_eval_iters:int = args.num_eval_iters
+    eval_freq:int = args.eval_freq
+    render:bool = ((not train_mode) or args.render) 
+    use_checkpoints:bool = args.use_checkpoints
 
     torch.manual_seed(2024)
-    gym.register_envs(ale_py)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Default v4 env doesn't handle frame flickering, so use gym wrappers to fix
-    env = gym.make("BreakoutNoFrameskip-v4", render_mode="human" if (not train_mode) or should_render else None)
-    env = gym.wrappers.AtariPreprocessing(env, scale_obs=True)
-    env = gym.wrappers.FrameStackObservation(env, stack_size=4)
-    env = gym.wrappers.ClipReward(env, min_reward=-1, max_reward=1)
-    env = gym.wrappers.NumpyToTorch(env, device=device)
+    log_dir = os.path.join(os.getcwd(), "logs", env_name, agent_name)
+    if not os.path.isdir(log_dir):
+        os.makedirs(log_dir)
+    videos_dir = os.path.join(log_dir, "videos")
+    if not os.path.isdir(videos_dir):
+        os.makedirs(videos_dir)
+    metrics_dir = os.path.join(log_dir, "metrics")
+    if not os.path.isdir(metrics_dir):
+        os.makedirs(metrics_dir)
+    figures_dir = os.path.join(log_dir, "figures")
+    if not os.path.isdir(figures_dir):
+        os.makedirs(figures_dir)
 
-    agent = Agent(num_actions=env.action_space.n.item(), device=device)
+    train_log_file_path = os.path.join(log_dir, "train_log.txt")
+    eval_log_file_path = os.path.join(log_dir, "eval_log.txt")
+    eval_rewards_file_path = os.path.join(metrics_dir, "eval_mean_rewards_per_episode.npy")
+    eval_rewards_figure_file_path = os.path.join(figures_dir, "eval_mean_rewards_per_episode.png")
 
+    env_in_default_registry = (env_name in gym.registry)
+    import ale_py
+    gym.register_envs(ale_py)
+    env_in_ale_registry = (env_name in gym.registry)
+    is_atari_env = env_in_ale_registry and not env_in_default_registry
     
+    env = gym.make(env_name, render_mode="human" if render else "rgb_array")
+    if not render:
+        env = gym.wrappers.RecordVideo(env, name_prefix="train", video_folder=videos_dir, step_trigger=lambda step:(step % (eval_freq * 4 if is_atari_env else eval_freq) == 0))
+    if is_atari_env:
+        env = gym.wrappers.AtariPreprocessing(env, scale_obs=True)
+        env = gym.wrappers.FrameStackObservation(env, stack_size=4)
+        env = gym.wrappers.ClipReward(env, min_reward=-1, max_reward=1)
+    env = gym.wrappers.NumpyToTorch(env, device=device)
+    env: gym.Env = env
+
+    agent = DQN(name=agent_name, env=env, device=device)
+
     if train_mode:
-        train(agent, env, log_file_path="train_log.txt")
+        train(agent, env, num_train_iters=num_train_iters, train_freq=train_freq, num_eval_iters=num_eval_iters, eval_freq=eval_freq, use_checkpoints=use_checkpoints, log_file_path=train_log_file_path, eval_rewards_file_path=eval_rewards_file_path, eval_rewards_figure_file_path=eval_rewards_figure_file_path)
     else:
-        eval(agent, env, log_file_path="eval_log.txt")
+        eval(agent, env, num_iters=num_eval_iters, use_checkpoints=use_checkpoints, log_file_path=eval_log_file_path)
     env.close()
 
 if __name__ == "__main__":
